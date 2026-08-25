7229bc1568be1e7b5901d0450066fe31c794390a
CheekuTheNonCoder <avisheksingh406@gmail.com>
2026-08-25 15:55:07 +0530

    update and fix

--- app.py
+++ app.py
@@ -69,6 +69,7 @@ REFUND_REASONS = [
     "DNR Order",
     "Delay in Delivery",
     "Order Cancelled by Seller",
+    "RTO" "Missing Item" "Quanity Mismatch" "Low Quality Product" "Colour Issue",
 ]
 
 REFUND_REASON_OPTIONS = [REFUND_REASON_PLACEHOLDER] + REFUND_REASONS
